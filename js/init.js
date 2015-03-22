30515eb05043bf8ff44641c317d8e741f3b17793
Geek4IT <chunjianye@gmail.com>
2015-03-22 09:46:11 +0800

    Update

--- js/init.js
+++ js/init.js
@@ -6,6 +6,24 @@
 
 (function($) {
 
+	var settings = {
+
+		// Header (homepage only)
+			header: {
+				fullScreen: true,
+				fadeIn: true,
+				fadeDelay: 500
+			},
+
+		// Carousels
+			carousels: {
+				speed: 4,
+				fadeIn: true,
+				fadeDelay: 250
+			},
+
+	};
+
 	skel.init({
 		reset: 'full',
 		breakpoints: {
@@ -48,6 +66,198 @@
 
 			}
 
+		// Carousels.
+			$('.carousel').each(function() {
+
+				var	$t = $(this),
+					$forward = $('<span class="forward"></span>'),
+					$backward = $('<span class="backward"></span>'),
+					$reel = $t.children('.reel'),
+					$items = $reel.children('article');
+
+				var	pos = 0,
+					leftLimit,
+					rightLimit,
+					itemWidth,
+					reelWidth,
+					timerId;
+
+				// Items.
+					if (settings.carousels.fadeIn) {
+
+						$items.addClass('loading');
+
+						$t.onVisible(function() {
+							var	timerId,
+								limit = $items.length - Math.ceil($window.width() / itemWidth);
+
+							timerId = window.setInterval(function() {
+								var x = $items.filter('.loading'), xf = x.first();
+
+								if (x.length <= limit) {
+
+									window.clearInterval(timerId);
+									$items.removeClass('loading');
+									return;
+
+								}
+
+								if (skel.vars.IEVersion < 10) {
+
+									xf.fadeTo(750, 1.0);
+									window.setTimeout(function() {
+										xf.removeClass('loading');
+									}, 50);
+
+								}
+								else
+									xf.removeClass('loading');
+
+							}, settings.carousels.fadeDelay);
+						}, 50);
+					}
+
+				// Main.
+					$t._update = function() {
+						pos = 0;
+						rightLimit = (-1 * reelWidth) + $window.width();
+						leftLimit = 0;
+						$t._updatePos();
+					};
+
+					if (skel.vars.IEVersion < 9)
+						$t._updatePos = function() { $reel.css('left', pos); };
+					else
+						$t._updatePos = function() { $reel.css('transform', 'translate(' + pos + 'px, 0)'); };
+
+				// Forward.
+					$forward
+						.appendTo($t)
+						.hide()
+						.mouseenter(function(e) {
+							timerId = window.setInterval(function() {
+								pos -= settings.carousels.speed;
+
+								if (pos <= rightLimit)
+								{
+									window.clearInterval(timerId);
+									pos = rightLimit;
+								}
+
+								$t._updatePos();
+							}, 10);
+						})
+						.mouseleave(function(e) {
+							window.clearInterval(timerId);
+						});
+
+				// Backward.
+					$backward
+						.appendTo($t)
+						.hide()
+						.mouseenter(function(e) {
+							timerId = window.setInterval(function() {
+								pos += settings.carousels.speed;
+
+								if (pos >= leftLimit) {
+
+									window.clearInterval(timerId);
+									pos = leftLimit;
+
+								}
+
+								$t._updatePos();
+							}, 10);
+						})
+						.mouseleave(function(e) {
+							window.clearInterval(timerId);
+						});
+
+				// Init.
+					$window.load(function() {
+
+						reelWidth = $reel[0].scrollWidth;
+
+						skel.change(function() {
+
+							if (skel.vars.isTouch) {
+
+								$reel
+									.css('overflow-y', 'hidden')
+									.css('overflow-x', 'scroll')
+									.scrollLeft(0);
+								$forward.hide();
+								$backward.hide();
+
+							}
+							else {
+
+								$reel
+									.css('overflow', 'visible')
+									.scrollLeft(0);
+								$forward.show();
+								$backward.show();
+
+							}
+
+							$t._update();
+						});
+
+						$window.resize(function() {
+							reelWidth = $reel[0].scrollWidth;
+							$t._update();
+						}).trigger('resize');
+
+					});
+
+			});
+
+		// Header.
+			if ($body.hasClass('homepage')) {
+
+				if (settings.header.fullScreen) {
+
+					$window.bind('resize.helios', function() {
+						window.setTimeout(function() {
+							var s = $header.children('.inner');
+							var sh = s.outerHeight(), hh = $window.height(), h = Math.ceil((hh - sh) / 2) + 1;
+
+							$header
+								.css('padding-top', h)
+								.css('padding-bottom', h);
+						}, 0);
+					}).trigger('resize');
+
+				}
+
+				if (settings.header.fadeIn) {
+
+					$.n33_preloadImage = function(url, onload) { var $img = $('<img />'), _IEVersion = (navigator.userAgent.match(/MSIE ([0-9]+)\./) ? parseInt(RegExp.$1) : 99); $img.attr('src', url); if ($img.get(0).complete || _IEVersion < 9) (onload)(); else $img.load(onload); };
+
+					$('<div class="overlay" />').appendTo($header);
+
+					$window
+						.load(function() {
+							var imageURL = $header.css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
+
+							$.n33_preloadImage(imageURL, function() {
+
+								if (skel.vars.IEVersion < 10)
+									$header.children('.overlay').fadeOut(2000);
+								else
+									window.setTimeout(function() {
+										$header.addClass('ready');
+									}, settings.header.fadeDelay);
+
+							});
+						});
+
+				}
+
+			}
+
 	});
+
+
 
 })(jQuery);
